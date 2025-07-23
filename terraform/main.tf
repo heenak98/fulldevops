1ff2b49e37ed8350e87e7d02449c1eb33cad7720
heenak98 <heenashaikh9483@gmail.com>
2025-07-23 03:24:54 +0000

    updated jenkins files

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ module "vpc" {
  
 module "eks_cluster" {
   source             = "./modules/eks_cluster"
-  cluster_name       = "my-eks-cluster-new12"
+  cluster_name       = "my-eks-cluster-new13"
   cluster_role_arn   = module.iam_cluster_role.iam_role_arn
   subnet_ids         = module.vpc.private_subnet_ids
   kubernetes_version = "1.27"
